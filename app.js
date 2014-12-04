4ee034d51b05d3964b217b1c322f400335bec3f7
Jake Billings <jake@eakjb.com>
2014-12-04 04:25:26 +0000

    Test for github pages

---
+++ app.js
@@ -0,0 +1,48 @@
+var app = angular.module("ninja.shout",["ngRoute","firebase"]);
+
+app.value("ninja.shout.fbURL", "https://eakjb-shout-ninja2.firebaseio.com/events");
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when( '/', {
+        controller: 'ninja.shout.index.main',
+        templateUrl: 'view_main.html'
+    })
+    .when( '/events/:event_id', {
+        controller: 'ninja.shout.index.event',
+        templateUrl: 'view_event.html'
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+});
+
+app.factory('ninja.shout.events', ["$firebase","ninja.shout.fbURL",function($firebase, fbURL) {
+    return $firebase(new Firebase(fbURL)).$asArray();
+}]);
+
+app.controller("ninja.shout.index.main",["$scope","$location","ninja.shout.events",function ($scope,$location,events) {
+    $scope.events=events;
+    
+    $scope.submitForm = function () {
+        $scope.events.$add($scope.formData);
+        $scope.resetForm();
+    };
+    $scope.resetForm = function () {
+        $scope.formData={
+            name:"",
+            description:""
+        };
+    };
+    $scope.deleteEvent = function (event) {
+        $scope.events.$remove(event);
+    };
+    $scope.detailEvent = function (event) {
+        $location.path('events/'+event.$id)
+    }
+    $scope.resetForm();
+}]);
+
+app.controller("ninja.shout.index.event",["$scope","$routeParams","ninja.shout.events",function($scope,$routeParams,events) {
+    $scope.event=events[$routeParams.event_id];
+}]);
