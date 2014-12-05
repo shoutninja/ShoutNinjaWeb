6913ec87af4f7848bee724d1438832e6fef34e43
Jake Billings <jake@eakjb.com>
2014-12-05 04:51:11 +0000

    Added chat

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-var app = angular.module("ninja.shout",["ngRoute","firebase"]);
+var app = angular.module("ninja.shout",["ngRoute","luegg.directives","firebase"]);
 
-app.value("ninja.shout.fbURL", "https://eakjb-shout-ninja2.firebaseio.com/events");
+app.constant("ninja.shout.constants.urls.firebase", "https://eakjb-shout-ninja2.firebaseio.com");
+
+app.service("ninja.shout.urls", ["ninja.shout.constants.urls.firebase", function (fbURL) {
+    this.events=fbURL+"/events";
+    this.chats=fbURL+"/chats"
+    this.settings=fbURL+"/settings"
+}]);
  
 app.config(function($routeProvider) {
     $routeProvider
@@ -19,11 +25,61 @@ app.config(function($routeProvider) {
     });
 });
 
-app.factory('ninja.shout.events', ["$firebase","ninja.shout.fbURL",function($firebase, fbURL) {
-    return $firebase(new Firebase(fbURL)).$asArray();
+app.factory("ninja.shout.api.settings", ["$firebase","ninja.shout.urls",function($firebase,urls) {
+    return $firebase(new Firebase(urls.settings)).$asObject();
 }]);
 
-app.controller("ninja.shout.index.events",["$scope","$location","ninja.shout.events",function ($scope,$location,events) {
+app.factory("ninja.shout.api.events", ["$firebase","ninja.shout.urls",function($firebase,urls) {
+    return $firebase(new Firebase(urls.events)).$asArray();
+}]);
+
+app.factory("ninja.shout.api.chats", ["$firebase","ninja.shout.api.settings","ninja.shout.urls",
+function($firebase,settings,urls) {
+    return $firebase(new Firebase(urls.chats)).$asArray();
+}]);
+
+app.service("ninja.shout.defaults", function () {
+    this.Chat=function() {
+        return {
+            //id
+            "text": "",
+            "user": {
+                //id
+                "username": "Guest"
+            }
+        };
+    };
+    this.Event=function () {
+        return {
+            "name":"",
+            "description":"",
+            "twitter":""
+        };
+    };
+});
+
+app.controller("ninja.shout.index.chats",["$scope","ninja.shout.defaults","ninja.shout.api.chats",
+function ($scope,defaults,chats) {
+    $scope.chats=chats;
+    
+    $scope.submitForm = function () {
+        $scope.chats.$add($scope.formData);
+    
+        //while (chats.length>10) {
+          //  chats.$remove(0);
+        //}
+    
+        $scope.resetForm();
+    };
+    $scope.resetForm = function () {
+        $scope.formData.text="";
+    };
+    
+    $scope.formData=new defaults.Chat();
+}]);
+
+app.controller("ninja.shout.index.events",["$scope","$location","ninja.shout.defaults","ninja.shout.api.events",
+function ($scope,$location,defaults,events) {
     $scope.events=events;
     
     $scope.submitForm = function () {
@@ -31,10 +87,7 @@ app.controller("ninja.shout.index.events",["$scope","$location","ninja.shout.eve
         $scope.resetForm();
     };
     $scope.resetForm = function () {
-        $scope.formData={
-            name:"",
-            description:""
-        };
+        $scope.formData=new defaults.Event;
     };
     $scope.deleteEvent = function (event) {
         $scope.events.$remove(event);
@@ -45,6 +98,6 @@ app.controller("ninja.shout.index.events",["$scope","$location","ninja.shout.eve
     $scope.resetForm();
 }]);
 
-app.controller("ninja.shout.index.event",["$scope","$routeParams","ninja.shout.events",function($scope,$routeParams,events) {
+app.controller("ninja.shout.index.event",["$scope","$routeParams","ninja.shout.api.events",function($scope,$routeParams,events) {
     $scope.event=events.$getRecord($routeParams.event_id);
 }]);
