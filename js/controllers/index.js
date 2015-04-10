fe701a1f0cfdac7a649000cf07eab7384c004fb7
jakebillings <jake@eakjb.com>
2015-04-09 20:16:41 -0600

    Added pie chart of websites

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -73,47 +73,97 @@ app.controller("ninja.shout.index.lynx", ["$scope", "$rootScope", "$window", "$l
 app.controller("ninja.shout.index.lynx.analysis", ["$scope", "$rootScope", "$routeParams", "$location", "ninja.shout.lynx.api.forum",
     function ($scope, $rootScope, $routeParams, $location, forum) {
         var now = new Date();
-        $scope.startDate = moment([now.getFullYear(),now.getMonth(),now.getDate()]).subtract(14, "days");
-        $scope.endDate = moment([now.getFullYear(),now.getMonth(),now.getDate()]).add(2, "days");
+        $scope.startDate = moment([now.getFullYear(), now.getMonth(), now.getDate()]).subtract(14, "days");
+        $scope.endDate = moment([now.getFullYear(), now.getMonth(), now.getDate()]).add(2, "days");
+
+        $scope.regex = new RegExp("((?!-)[A-Za-z0-9-]{1,63}(?!-)\\.)+[A-Za-z]{2,6}");
+        $scope.domainCount = 5;
+
+        $scope.pieChartColorTags = {
+            "www.youtube.com": "red",
+            "www.bing.com": "blue",
+            "www.google.com": "green"
+        };
 
         $scope.forEachInRange = function (callback) {
             angular.forEach(forum, function (post) {
                 var postDate = moment(new Date(post.timestamp));
                 if (postDate.diff($scope.startDate) >= 0 &&
                     postDate.diff($scope.endDate) < 0) {
-                    callback(post,postDate);
+                    callback(post, postDate);
                 }
             });
         };
 
         $scope.setChartType = function (chartType) {
-            $location.path("/lynx/analysis/"+chartType);
+            $location.path("/lynx/analysis/" + chartType);
         };
 
         $scope.chartTypes = {
-            pie:{
+            pie: {
                 name: "pie",
-                update: function() {
-                    $scope.forEachInRange(function(post) {
-                        $scope.data.filter(function(item) {
-                            return post.match("http://.*");
-                        });
+                displayName: "Pie",
+                update: function () {
+                    var data = {};
+
+                    $scope.forEachInRange(function (post) {
+
+                        var domain = $scope.regex.exec(post.url)[0];
+                        if (!data[domain]) {
+                            data[domain] = 1;
+                        } else {
+                            data[domain]++;
+                        }
                     });
+
+                    var otherCount;
+                    var formattedData;
+                    var minimumSubmissions = 2;
+
+                    do {
+                        console.log($scope.domainCount+","+minimumSubmissions);
+                        otherCount=0;
+                        formattedData = [];
+
+                        angular.forEach(Object.keys(data), function (key) {
+                            if (data[key] > minimumSubmissions) {
+                                var color;
+                                if (Object.keys($scope.pieChartColorTags).indexOf(key) > -1) {
+                                    color = $scope.pieChartColorTags[key];
+                                } else {
+                                    color = "rgb("+Math.floor(Math.random()*256)+", "+
+                                    Math.floor(Math.random()*256)+", "+
+                                    Math.floor(Math.random()*256)+")";
+                                }
+
+                                formattedData.push({label: key, value: data[key], color: color});
+                            } else {
+                                otherCount++;
+                            }
+                        });
+
+                        minimumSubmissions++;
+                    } while (formattedData.length>$scope.domainCount&&minimumSubmissions>1);
+
+                    formattedData.push({label:"Other", value:otherCount, color: "#7E7E7E"});
+
+                    $scope.data = formattedData;
 
                     $scope.options = {thickness: 100};
                 }
             },
-            line:{
+            line: {
                 name: "line",
-                update: function() {
-                    if (forum.length<1) return;
+                displayName: "Line",
+                update: function () {
+                    if (forum.length < 1) return;
 
                     var numDays = $scope.endDate.diff($scope.startDate, "days");
                     for (var i = 0; i < numDays; i++) {
                         $scope.data[i] = {x: i, value: 0};
                     }
 
-                    $scope.forEachInRange(function (post,postDate) {
+                    $scope.forEachInRange(function (post, postDate) {
                         $scope.data[postDate.diff($scope.startDate, "days")].value++;
                     });
 
@@ -141,8 +191,10 @@ app.controller("ninja.shout.index.lynx.analysis", ["$scope", "$rootScope", "$rou
                         },
                         lineMode: "linear",
                         tension: 0.7,
-                        tooltip: {mode: "scrubber", formatter: function(x, y, series) {
-                            return $scope.startDate.clone().add(x, "days").format("ddd MM/DD/YYYY") + ": " + y}
+                        tooltip: {
+                            mode: "scrubber", formatter: function (x, y, series) {
+                                return $scope.startDate.clone().add(x, "days").format("ddd MM/DD/YYYY") + ": " + y
+                            }
                         },
                         drawLegend: true,
                         drawDots: true,
@@ -153,12 +205,16 @@ app.controller("ninja.shout.index.lynx.analysis", ["$scope", "$rootScope", "$rou
         };
 
         $rootScope.$watch(function () {
-            return JSON.stringify(forum)+$routeParams.chartType+$scope.startDate+$scope.endDate;
+            return JSON.stringify(forum) + $routeParams.chartType +
+                $scope.startDate +
+                $scope.endDate +
+                $scope.regex +
+                $scope.domainCount;
         }, function () {
             $scope.data = [];
-            $scope.chartType=$routeParams.chartType;
+            $scope.chartType = $routeParams.chartType;
 
-            if (Object.keys($scope.chartTypes).indexOf($routeParams.chartType)>-1)
-            $scope.chartTypes[$routeParams.chartType].update();
+            if (forum.length>0 && Object.keys($scope.chartTypes).indexOf($routeParams.chartType) > -1)
+                $scope.chartTypes[$routeParams.chartType].update();
         });
     }]);
